11f7d031d35154d9fee15b5da6ee905211226e3f
Claude Wilder <claude.wilder@gmail.com>
2024-12-26 08:34:35 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// Register the Service Worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(() => console.log('Service Worker Registered'))
+        .catch(err => console.error('Service Worker Registration Failed', err));
+}
---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'static-image-cache-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/qr-bw.png',
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Caching assets');
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
